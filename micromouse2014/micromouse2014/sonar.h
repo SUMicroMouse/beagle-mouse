7077972ada6e534ed0ead0279f2a3aaf6794d497
lylemoffitt <lyle@moffitt.com>
2014-01-21 17:30:38 -0500

    Added sonar class for additional sensors

---
+++ micromouse2014/micromouse2014/sonar.h
@@ -0,0 +1,14 @@
+//
+//  sonar.h
+//  micromouse2014
+//
+//  Created by Lyle Moffitt on 1/21/14.
+//  Copyright (c) 2014 Lyle Moffitt. All rights reserved.
+//
+
+#ifndef __micromouse2014__sonar__
+#define __micromouse2014__sonar__
+
+#include <iostream>
+
+#endif /* defined(__micromouse2014__sonar__) */
