9bd6aba31212f0dcdc058f71cc10240d4a2826ea
lylemoffitt <lyle@moffitt.com>
2014-01-21 14:44:25 -0500

    Re-edit. Basic initial commit

---
+++ micromouse2014/micromouse2014/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  micromouse2014
+//
+// Created by Lyle Moffitt, Carlton Semple, & Nick Beni
+// Created at Syracuse University for the 2014 micromouse competition
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
