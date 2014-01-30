1239308c4982f8f2d12b84b01ca7fb5419d5cf72
iTchyBandit <csemp@outlook.com>
2014-01-30 01:50:02 -0500

    finally starting

--- micromouse2014/micromouse2014/star.cpp
+++ micromouse2014/micromouse2014/star.cpp
@@ -2,9 +2,83 @@
 //  star.cpp
 //  micromouse2014
 //
-//  Created by Lyle Moffitt on 1/21/14.
-//  Copyright (c) 2014 Lyle Moffitt. All rights reserved.
 //
 
 #include "star.h"
 
+local_grid::local_grid()
+{
+	viewfinder = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	previous = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+}
+
+void local_grid::updateView(grid &fetched_scan, int &returnedLeft, int &returnedRight, double &returnedFront)
+{
+	// insert math here. depending on angle, assign to appropriate location in vector
+	
+	// much of the viewFinder ideally should have consistent values, assuming a perfectly straight line down the middle of the cells. 
+	// a fluctuation greater than a to-be-determined number will trigger action
+
+	// hopefully there's a more efficient way to check, but for now....
+	double check_left, check_right;
+	for (int i = 0; i < viewfinder.size(); i++)
+	{
+		// if the ends report great distances, then it's safe to assume that they're open spaces to the sides
+	}
+
+	// simple check for the center/front
+	double front_change = viewfinder[9] - previous[9];
+	if (front_change < 0)
+		returnedFront = front_change; // some wall is coming up
+}
+
+/********** Star ********/
+star::star()
+{
+	// size of cells
+	lengthwidth = 16; // free to change
+	threshold = 2;
+
+	leftFromViewfinder = 0;
+	rightFromViewfinder = 0;
+	frontFromViewfinder = 0;
+}
+
+
+void star::motion(grid &fetched_scan)
+{
+	// constantly update the view as long as the mouse is moving
+	while (true)
+	{
+		viewFinder.updateView(fetched_scan, leftFromViewfinder, rightFromViewfinder, frontFromViewfinder);
+
+		if ((leftFromViewfinder > threshold) && (rightFromViewfinder > threshold))
+		{ // both sides are open, so move forward and then guess
+
+			// motion forward 
+			do{
+				viewFinder.updateView(fetched_scan, leftFromViewfinder, rightFromViewfinder, frontFromViewfinder);
+			} while (frontFromViewfinder > threshold); // threshold for front should probably be the same value
+
+			// guess
+		}
+		else if (leftFromViewfinder > threshold)
+		{
+			// motion forward .				********************* might need to change to a while loop *********************
+			do{
+				viewFinder.updateView(fetched_scan, leftFromViewfinder, rightFromViewfinder, frontFromViewfinder);
+			} while (frontFromViewfinder > threshold); // threshold for front should probably be the same value
+
+			// Turn left
+		}
+		else if (rightFromViewfinder > threshold)
+		{
+			// motion forward 
+			do{
+				viewFinder.updateView(fetched_scan, leftFromViewfinder, rightFromViewfinder, frontFromViewfinder);
+			} while (frontFromViewfinder > threshold); // threshold for front should probably be the same value
+
+			// Turn right
+		}
+	}
+}
--- micromouse2014/micromouse2014/star.h
+++ micromouse2014/micromouse2014/star.h
@@ -2,14 +2,13 @@
 //  star.h
 //  micromouse2014
 //
-//  Created by Lyle Moffitt on 1/21/14.
-//  Copyright (c) 2014 Lyle Moffitt. All rights reserved.
 //
 
 #ifndef __micromouse2014__star__
 #define __micromouse2014__star__
 
 #include <iostream>
+#include <vector>
 #include <stack>
 
 
@@ -19,17 +18,48 @@
 typedef
 std::stack<grid &> path;
 
+// contains 
+class item{
+
+};
+
+/* This will simplify data input into 19 regions. 
+The angle of the data from the lidar will determine the position in viewfinder, 
+	and the distance/radius will be used to determine when an open space is coming up on the left/right, or a wall ahead.
+The number of items in the viewfinder may be increased for better accuracy and increased response time*/
+class local_grid{
+	std::vector<double> viewfinder;
+	std::vector<double> previous;
+public:
+	local_grid();
+
+	// takes the grid from the lidar scan and simplifies the data
+	void updateView(grid &fetched_scan, int &returnedLeft, int &returnedRight, double &returnedFront); 
+};
+
+
 class star
 {
-	grid	maze;
+	// size of cells
+	double lengthwidth;
+	double threshold; // distance at which it's determined the side walls aren't there
+
+	grid maze;
 	
-	path	to_orig;
-	path	guess;
-	
+	path to_orig;
+	path guess;
+
+	/*********** Local Grid ************/
+	int leftFromViewfinder; // will get the region that the open space (to the left) is currently at in relation to the lidar
+	int rightFromViewfinder; // will get the region that the open space (to the right) is currently at in relation to the lidar
+	double frontFromViewfinder; // gets the distance of the region directly in front
 public:
 	star();
+
+	grid *get_scan(); // retrieves scan from lidar 
+	void motion(grid &fetched_scan); // overall motion. calls the turn functions
 	
-	
+	local_grid viewFinder;
 };
 
 #endif /* defined(__micromouse2014__star__) */
