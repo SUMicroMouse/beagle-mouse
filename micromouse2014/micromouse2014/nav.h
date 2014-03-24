ce1af40b076f99ea1453b7893d5a7a81858aa71e
lylemoffitt <lyle@moffitt.com>
2014-03-23 23:30:07 -0400

    nav is in the wrong directory

--- micromouse2014/micromouse2014/nav.h
+++ micromouse2014/micromouse2014/nav.h
@@ -1,19 +1,49 @@
 #include <iostream>
 #include "motor.h"
+#include "lidar.h"
+#include "packet.h"
+
+
 
 using namespace std;
 
+namespace nav_config
+{
+	const double max_speed = 1; 
+	const double min_speed = 0.5;
+	const double min_negspeed = 0.5;
+	const double max_negspeed = 0;
+	const double max_invariance = 0.0001;
+	const double min_invariance = 0.00000001;
+	const size_t duration = 10000000;
+	
+
+
+}
+//change
+
 class nav
 {
-<<<<<<< HEAD
 private:
 	motor left; 
 	motor right;
-=======
-    
-
->>>>>>> 584141a692497396a4241499915ffccd4f264129
+	lidar &lidar_pointer;
+	timestamp last_distance; //timestamp of last distance 
 public:
+	
+	nav(lidar &l_pointer):lidar_pointer(l_pointer){} //constructor
+
+
+
+	//checks to see if wheels are in the same posistion 
+	 void synchronize(double speed);
+	 
+	// assuming the front middle is 0 degrees
+	void turn(double angle);
+
+	// go forward in a small enough chunk (one cell approx.) so that the distance to the opposing wall can be scanned/updated
+	void goForwardOne();
+
 	//breaks the right motor, left motor moves forward 
 	void curveleft(float VR, float speed);
 	
@@ -27,10 +57,10 @@ public:
 	void turnright(float VL, float VR);
 	
 	//moves the rover forward 
-	void moveforward(float VL, float VR);
+	void moveforward();
 	
 	//moves the rover backwards
-	void movebackward(float VL,float VR);
+	void movebackward();
 	
 	//spins the rover around 180 deg
 	void turnaround(float VR, float VL);
@@ -42,16 +72,18 @@ public:
 	void veerright(float VL, float VR);
 	
 	//accelerates the rover forward  
-	void XLR8(float VL, float VR);
+	void XLR8(double a);
 	
 	//cuts speed down? 
-	void coast(float VL, float VR);
+	void coast();
 	
 	//stops motor 
-	void stop(float VL, float VR);
+	void stop();
 	
 	//moves a distance in terms of mm 
 	void movedistancevariable(float VL, float VR);
+
+	void getdistance_forward(); // fetches the distance forward
 	
 };
 
