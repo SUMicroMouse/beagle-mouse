077a5a5ff0c41ce7c2f341a4c703354cf7ed23a9
Mark-Skinner <skinz93@optonline.net>
2015-02-23 00:52:34 -0500

    .

--- micromouse2014/micromouse2014/Room.h
+++ micromouse2014/micromouse2014/Room.h
@@ -6,10 +6,8 @@
 #undef max
 
 #include <vector>
-//#include "Maze.h"
-#include "Wall.h"
+#include "wall.h"
 #include "Location.h"
-
 
 // Designates where the walls are
 enum RoomType
@@ -22,7 +20,7 @@ enum RoomType
 class Room
 {
 private:
-	std::vector<Wall> openings; // Vector of Wall objects
+	std::vector<Wall*> openings; // Vector of Wall objects
 	int opens; // Number of openings
 	Location loc;
 	RoomType type;
@@ -32,65 +30,18 @@ private:
 
 	int breadth_heuristic;
 	void reset_breadth_heuristic();
-	//void set_adjacent_rooms(Maze &maze);
+	//void set_adjacent_rooms(Room(&maze)[16][16]);
 
 public:
-	Room() :openings(4), opens(0), loc(0,0)
-	{ 
-		breadth_heuristic = std::numeric_limits<int>::max(); 
+	Room() :opens(0), loc(0,0)
+	{
+		for (int i = 0; i < 4; i++)
+			openings.push_back(new Wall());
+		breadth_heuristic = std::numeric_limits<int>::max();
 		type = RoomType::NONE;
 	}
 
-	Room(int v, int x, int y) :openings(4), opens(0), loc(x,y)
-	{
-		// Variable v determine which kind of room will be made.
-		// Why did I name it v? I have no idea..
-		/*
-		char topLeftCorner = 201;
-		char topRightCorner = 187;
-		char bottomLeftCorner = 200;
-		char bottomRightCorner = 188;
-		char hoLine = 205;
-		char vertLine = 186;
-		*/
-		type = (RoomType)(v - 1);
-		switch (v)
-		{
-		case 1: // none
-			opens = 4; break;
-		case 2: // left
-			openings[0].setClosed(1); opens = 3; break;
-		case 3: // bottom
-			openings[1].setClosed(1); opens = 3; break;
-		case 4: // right
-			openings[2].setClosed(1); opens = 3; break;
-		case 5: // top
-			openings[3].setClosed(1); opens = 3; break;
-		case 6: // left bottom
-			openings[0].setClosed(1); openings[1].setClosed(1); opens = 2; break;
-		case 7: // bottom right
-			openings[1].setClosed(1); openings[2].setClosed(1); opens = 2; break;
-		case 8: // left right
-			openings[0].setClosed(1); openings[2].setClosed(1); opens = 2; break;
-		case 9: // left top
-			openings[0].setClosed(1); openings[3].setClosed(1); opens = 2; break;
-		case 10: // bottom top
-			openings[1].setClosed(1); openings[3].setClosed(1); opens = 2; break;
-		case 11: // right top
-			openings[2].setClosed(1); openings[3].setClosed(1); opens = 2; break;
-		case 12: // left bottom right
-			openings[0].setClosed(1); openings[1].setClosed(1); openings[2].setClosed(1); opens = 1; break;
-		case 13: // left bottom top
-			openings[0].setClosed(1); openings[1].setClosed(1); openings[3].setClosed(1); opens = 1; break;
-		case 14: // left right top
-			openings[0].setClosed(1); openings[2].setClosed(1); openings[3].setClosed(1); opens = 1; break;
-		case 15: // bottom right top
-			openings[1].setClosed(1); openings[2].setClosed(1); openings[3].setClosed(1); opens = 1; break;
-
-		default:
-			for (int i = 0; i < 4; i++) { openings[i].setClosed(1); } break;
-		}
-	}
+	Room(int v, int x, int y);
 
 	std::vector<bool> getOpenings();
 
@@ -102,12 +53,9 @@ public:
 	bool checked;						// boolean used in depth first search to prevent looping
 	int getPassages(){ return opens; }
 	char* getRoom();
-
 	void setWall(int side, bool value);
 
 	void operator =(Room & room2);
 };
-
-
 
 #endif
